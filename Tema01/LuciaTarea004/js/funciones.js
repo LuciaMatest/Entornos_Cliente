040986f3e8deb0f26793f6c95e128bf96f30418b
Lucia <lucia.matest@educa.jcyl.es>
2022-10-07 15:14:10 +0000

    1

--- Tema01/LuciaTarea004/js/funciones.js
+++ Tema01/LuciaTarea004/js/funciones.js
@@ -1,13 +1,53 @@
 "use strict";
+/*Area de un circulo*/
 function area_circulo(radio) {
-    if(typeof radio == 'string'){
-        alert("No ha introducido un número");
-    }
     return Math.PI * radio ** 2
 }
 
 try {
-    console.log(area_circulo());
+    let radio = 4;
+    console.log(`El area del circulo con radio ${radio} es ${area_circulo(radio)}`);
+} catch (error) {
+    alert("FALLO");
+}
+
+/*Perimetro de una circunferencia*/
+function peri_circulo(radio) {
+    return Math.PI * radio * 2
+}
+
+try {
+    let radio = 5.6;
+    console.log(`El perimetro del circulo con radio ${radio} es ${peri_circulo(radio)}`);
+} catch (error) {
+    alert("FALLO");
+}
+
+/*Perimetro de un poligono regular*/
+function peri_poligono(long, lados) {
+    return long * lados
+}
+
+try {
+    let long = 8;
+    let lados = 5;
+    console.log(`El perimetro de un poligono con ${lados} lados con longitud ${long} es ${peri_poligono(long, lados)}`);
+} catch (error) {
+    alert("FALLO");
+}
+
+/*Area de un poligono regular*/
+function area_poligono(long, lados) {
+    let perimetro = long * lados;
+    let alfa = 360 / lados;
+    let apotema = (long / 2) / Math.tan((alfa / 2))
+    return (perimetro * apotema) / 2
+}
+
+try {
+    let long = 8;
+    let lados = 5;
+    console.log(`El area de un poligono con ${lados} lados y longitud ${long} es ${area_poligono(long, lados)}`);
 } catch (error) {
     alert("FALLO");
 }
