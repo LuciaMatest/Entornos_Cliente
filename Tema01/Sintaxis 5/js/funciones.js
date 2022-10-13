3835259f435e550fbc9b5047dfca0040376dd875
Lucia <lucia.matest@educa.jcyl.es>
2022-10-13 08:11:51 +0000

    1

--- Tema01/Sintaxis 5/js/funciones.js
+++ Tema01/Sintaxis 5/js/funciones.js
@@ -1,10 +1,19 @@
-let a = ['Lunes', 'Martes', 2, 4, 6, ['Lucia', 3, 5, 6, 7, 8]];
-let n = 46;
+let a = [2, 5, "pepe", ['Lucia', 24, [5, 9]]];
+
 console.log(a);
-console.log(a[5][0].length);
+
+console.log(a[3][2].length);
 
 a.push('Santiago');
 console.log(a);
 
 a.unshift('Daniel');
+console.log(a);
+
+a[4][2].push(0);
+console.log(a);
+
+const eliminado1= a.pop();
+a.pop();
+a.push(eliminado1);
 console.log(a);
