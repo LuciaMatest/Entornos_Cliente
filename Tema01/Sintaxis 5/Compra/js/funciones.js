d892bc372203d3effdd3e264fd04e2af9b3f4ae3
Lucia <lucia.matest@educa.jcyl.es>
2022-10-13 08:41:52 +0000

    1

---
+++ Tema01/Sintaxis 5/Compra/js/funciones.js
@@ -0,0 +1,11 @@
+let frutas = ['Peras', 'Manzanas', 'Kiwis', 'Platanos', 'Mandarinas'];
+console.log(frutas);
+
+let eliminar = frutas.splice(1,1);
+console.log(frutas);
+
+frutas.splice(3,0, 'Naranjas','Sandias');
+console.log(frutas);
+
+frutas.splice(1,1, 'Cerezas','Nisperos');
+console.log(frutas);
