680c10f3d21d49be1a18236faa46849446874c4a
Lucia <lucia.matest@educa.jcyl.es>
2022-10-14 10:07:06 +0000

    1

--- Tema01/Sintaxis 5/js/funciones.js
+++ Tema01/Sintaxis 5/js/funciones.js
@@ -1,20 +1,35 @@
-let a = [2, 5, "pepe", ['Lucia', 24, [5, 9]]];
+// let a = [2, 5, "pepe", ['Lucia', 24, [5, 9]]];
 
+// console.log(a);
+
+// console.log(a[3][2].length);
+
+// a.push('Santiago');
+// console.log(a);
+
+// a.unshift('Daniel');
+// console.log(a);
+
+// a[4][2].push(0);
+// console.log(a);
+
+// const eliminado1= a.pop();
+// a.pop();
+// a.push(eliminado1);
+// console.log(a);
+
+function añadirMarzo() {
+    document.write('<ul> <li> Enero </li> <li> Febrero </li> <li> Marzo </li>  </ul>');
+}
+
+añadirMarzo();
+
+let a = ['hola','adios','Bien','Mal']
 console.log(a);
-
-console.log(a[3][2].length);
-
-a.push('Santiago');
-console.log(a);
-
-a.unshift('Daniel');
-console.log(a);
-
-a[4][2].push(0);
-console.log(a);
-
-const eliminado1= a.pop();
-a.pop();
-a.push(eliminado1);
-console.log(a);
+let b = a.sort(function(elem1, elem2) {
+    if (elem1.toLocaleLowerCase() < elem2.toLocaleLowerCase()) return -1;
+    if (elem1.toLocaleLowerCase() > elem2.toLocaleLowerCase()) return 1;
+    if (elem1.toLocaleLowerCase() = elem2.toLocaleLowerCase()) return 0;
+    });
+console.log(b);
 
