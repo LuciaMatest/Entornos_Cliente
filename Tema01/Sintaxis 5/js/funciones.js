a0f4ea0f3f56b06cd607a7f39088e6a4f245f488
Lucia <lucia.matest@educa.jcyl.es>
2022-10-14 10:19:28 +0000

    1

--- Tema01/Sintaxis 5/js/funciones.js
+++ Tema01/Sintaxis 5/js/funciones.js
@@ -18,18 +18,21 @@
 // a.push(eliminado1);
 // console.log(a);
 
-function añadirMarzo() {
-    document.write('<ul> <li> Enero </li> <li> Febrero </li> <li> Marzo </li>  </ul>');
-}
+// function añadirMarzo() {
+//     document.write('<ul> <li> Enero </li> <li> Febrero </li> <li> Marzo </li>  </ul>');
+// }
 
-añadirMarzo();
+// añadirMarzo();
 
-let a = ['hola','adios','Bien','Mal']
-console.log(a);
-let b = a.sort(function(elem1, elem2) {
-    if (elem1.toLocaleLowerCase() < elem2.toLocaleLowerCase()) return -1;
-    if (elem1.toLocaleLowerCase() > elem2.toLocaleLowerCase()) return 1;
-    if (elem1.toLocaleLowerCase() = elem2.toLocaleLowerCase()) return 0;
-    });
-console.log(b);
+// let a = ['hola','adios','Bien','Mal']
+// console.log(a);
+// let b = a.sort(function(elem1, elem2) {
+//     if (elem1.toLocaleLowerCase() < elem2.toLocaleLowerCase()) return -1;
+//     if (elem1.toLocaleLowerCase() > elem2.toLocaleLowerCase()) return 1;
+//     if (elem1.toLocaleLowerCase() = elem2.toLocaleLowerCase()) return 0;
+//     });
+// console.log(b);
 
+
+let numeros = [4,8,3,10,5];
+console.log(numeros.sort((n1, n2) => n1-n2));
