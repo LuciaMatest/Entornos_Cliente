bcca903e43e0a7350dcab7377622f8e453acec75
Lucia <lucia.matest@educa.jcyl.es>
2022-10-15 17:37:05 +0000

    1

--- Tema01/Sintaxis 5/Meses/js/funciones.js
+++ Tema01/Sintaxis 5/Meses/js/funciones.js
@@ -1,3 +1,10 @@
-function añadirMeses() {
-    
-}
+const arrayMeses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+
+document.querySelector("#añadir").addEventListener("click", () => {
+    if (arrayMeses.indexOf(meses.textContent) === -1 || arrayMeses.indexOf(meses.textContent) === arrayMeses.length - 1) {
+        meses.textContent = arrayMeses[0];
+    } else {
+        meses.textContent = arrayMeses[arrayMeses.indexOf(meses.textContent) + 1];
+    }
+});
