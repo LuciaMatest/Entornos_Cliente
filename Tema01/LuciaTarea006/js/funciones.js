b77517b0f6eb868841a6ffe8017222f7a59a94c3
Lucia <lucia.matest@educa.jcyl.es>
2022-10-25 18:23:34 +0000

    1

--- Tema01/LuciaTarea006/js/funciones.js
+++ Tema01/LuciaTarea006/js/funciones.js
@@ -1,14 +1,29 @@
 function rombo(a) {
+    let filas = parseInt(document.getElementById(''))
     for (let i = 1; i <= a; i++) {
-        for (let j = 1; j <= i; j++) {
-            document.write("*");
+        for (let j = 1; j <= a - i; j++) {
+            document.write("&nbsp;&nbsp;");
+        }
+        for (let h = 1; h <= 2 * i - 1; h++) {
+            if (h == 1 || h == 2 * i - 1) {
+                document.write("*");
+            } else {
+                document.write("&nbsp;&nbsp;");
+            }
         }
         document.write("<br>");
     }
-
-    for (let i = a - 1; i > 0; i--) {
-        for (let j = i; j > 0; j--) {
-            document.write("*");
+    a--;
+    for (i = 1; i <= a; i++) {
+        for (j = 1; j <= i; j++) {
+            document.write("&nbsp;&nbsp;");
+        }
+        for (h = 1; h <= (a - i) * 2 + 1; h++) {
+            if (h == 1 || h == (a - i) * 2 + 1) {
+                document.write("*");
+            } else {
+                document.write("&nbsp;&nbsp;");
+            }
         }
         document.write("<br>");
     }
@@ -16,31 +31,3 @@ function rombo(a) {
 }
 
 rombo(5);
-
-for (let i = 1; i <= a; i++) {
-    for (let j = 1; j <= a - i; j++) {
-        document.write("*");
-    }
-    for (let h = 1; h <= 2 * i - 1; h++) {
-        if (h == 1 || h == 2 * i - 1) {
-            document.write(cadena);
-        } else {
-            document.write(" ");
-        }
-    }
-    document.write("<br>");
-}
-a--;
-for (i = 1; i <= a; i++) {
-    for (j = 1; j <= i; j++) {
-        document.write(piramide + " <br>");
-    }
-    for (h = 1; h <= (a - i) * 2 + 1; h++) {
-        if (h == 1 || h == (a - i) * 2 + 1) {
-            document.write(cadena);
-        } else {
-            document.write(piramide + " <br>");
-        }
-    }
-    document.write("<br>");
-}
