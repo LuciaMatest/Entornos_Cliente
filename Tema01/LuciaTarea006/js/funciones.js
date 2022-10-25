7dfcf6a3e51422f34ff6bfb9dcb096b3e5894609
Lucia <lucia.matest@educa.jcyl.es>
2022-10-25 18:40:13 +0000

    1

--- Tema01/LuciaTarea006/js/funciones.js
+++ Tema01/LuciaTarea006/js/funciones.js
@@ -1,33 +1,30 @@
-function rombo(a) {
-    let filas = parseInt(document.getElementById(''))
+function rombo() {
+    let a = parseInt(document.getElementById('idFilas').value);
     for (let i = 1; i <= a; i++) {
         for (let j = 1; j <= a - i; j++) {
-            document.write("&nbsp;&nbsp;");
+            document.body.innerHTML += "&nbsp;&nbsp;";
         }
         for (let h = 1; h <= 2 * i - 1; h++) {
             if (h == 1 || h == 2 * i - 1) {
-                document.write("*");
+                document.body.innerHTML += "*";
             } else {
-                document.write("&nbsp;&nbsp;");
+                document.body.innerHTML += "&nbsp;&nbsp;";
             }
         }
-        document.write("<br>");
+        document.body.innerHTML += "<br>";
     }
     a--;
     for (i = 1; i <= a; i++) {
         for (j = 1; j <= i; j++) {
-            document.write("&nbsp;&nbsp;");
+            document.body.innerHTML += "&nbsp;&nbsp;";
         }
         for (h = 1; h <= (a - i) * 2 + 1; h++) {
             if (h == 1 || h == (a - i) * 2 + 1) {
-                document.write("*");
+                document.body.innerHTML += "*";
             } else {
-                document.write("&nbsp;&nbsp;");
+                document.body.innerHTML += "&nbsp;&nbsp;";
             }
         }
-        document.write("<br>");
+        document.body.innerHTML += "<br>";
     }
-    // var elem = document.getElementById('idRombo');
 }
-
-rombo(5);
