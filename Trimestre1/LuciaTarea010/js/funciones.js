83b6be43e598fbe3cf4a2077d9745a194b9ac01b
Lucia <lucia.matest@educa.jcyl.es>
2022-11-07 11:46:39 +0000

    1

--- Trimestre1/LuciaTarea010/js/funciones.js
+++ Trimestre1/LuciaTarea010/js/funciones.js
@@ -1,19 +1,19 @@
 let contador = 1;
-        //funciones para que cada celda al darle doble click nos ponga el color que queremos 
-        document.getElementById("azul").addEventListener("dblclick", function () {
-            //contador
-            this.innerHTML = `${contador++} - A`;
-            this.style.backgroundColor = "cornflowerblue";
-        });
-        document.getElementById("negro").addEventListener("dblclick", function () {
-            this.innerHTML = `${contador++} - B`;
-            this.style.backgroundColor = "black";
-        });
-        document.getElementById("crimson").addEventListener("dblclick", function () {
-            this.innerHTML = `${contador++} - C`;
-            this.style.backgroundColor = "crimson";
-        });
-        document.getElementById("dorado").addEventListener("dblclick", function () {
-            this.innerHTML = `${contador++} - D`;
-            this.style.backgroundColor = "darkgoldenrod";
-        });
+//funciones para que cada celda al darle doble click nos ponga el color que queremos 
+document.getElementById("azul").addEventListener("dblclick", function () {
+    //contador
+    this.innerHTML = `${contador++} - A`;
+    this.style.backgroundColor = "cornflowerblue";
+});
+document.getElementById("negro").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - B`;
+    this.style.backgroundColor = "black";
+});
+document.getElementById("crimson").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - C`;
+    this.style.backgroundColor = "crimson";
+});
+document.getElementById("dorado").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - D`;
+    this.style.backgroundColor = "darkgoldenrod";
+});
