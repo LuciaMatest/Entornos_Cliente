287b787a5426a201b996b22c9908feb86ba8ea5e
Lucia <lucia.matest@educa.jcyl.es>
2022-11-07 11:35:10 +0000

    1

---
+++ Trimestre1/LuciaTarea009/js/funciones.js
@@ -0,0 +1,13 @@
+//funciones para que cada celda al darle doble click nos ponga el color que queremos 
+document.getElementById("azul").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "cornflowerblue";
+});
+document.getElementById("negro").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "black";
+});
+document.getElementById("crimson").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "crimson";
+});
+document.getElementById("dorado").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "darkgoldenrod";
+});
