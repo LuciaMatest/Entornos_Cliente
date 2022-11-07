9b5625a5d4a9d3d24aa712983c2b518eb08ebed1
Lucia <lucia.matest@educa.jcyl.es>
2022-11-07 12:23:27 +0000

    1

--- Trimestre1/LuciaTarea010/js/funciones.js
+++ Trimestre1/LuciaTarea010/js/funciones.js
@@ -1,37 +1,38 @@
-// let contador = 1;
-// //funciones para que cada celda al darle doble click nos ponga el color que queremos 
-// document.getElementById("azul").addEventListener("dblclick", function () {
-//     //contador
-//     this.innerHTML = `${contador++} - A`;
-//     this.style.backgroundColor = "cornflowerblue";
-// });
-// document.getElementById("negro").addEventListener("dblclick", function () {
-//     this.innerHTML = `${contador++} - B`;
-//     this.style.backgroundColor = "black";
-// });
-// document.getElementById("crimson").addEventListener("dblclick", function () {
-//     this.innerHTML = `${contador++} - C`;
-//     this.style.backgroundColor = "crimson";
-// });
-// document.getElementById("dorado").addEventListener("dblclick", function () {
-//     this.innerHTML = `${contador++} - D`;
-//     this.style.backgroundColor = "darkgoldenrod";
-// });
+let contador = 1;
+//funciones para que cada celda al darle doble click nos ponga el color que queremos 
+document.getElementById("azul").addEventListener("dblclick", function () {
+    //contador
+    this.innerHTML = `${contador++} - A`;
+    this.style.backgroundColor = "cornflowerblue";
+});
+document.getElementById("negro").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - B`;
+    this.style.backgroundColor = "black";
+});
+document.getElementById("crimson").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - C`;
+    this.style.backgroundColor = "crimson";
+});
+document.getElementById("dorado").addEventListener("dblclick", function () {
+    this.innerHTML = `${contador++} - D`;
+    this.style.backgroundColor = "darkgoldenrod";
+});
 
-//Respuesta clase con array
-'use strict';
+// //Respuesta clase con array
+// 'use strict';
 
-const ARRAY_CELDAS = Array.from(document.getElementsByTagName('td'));
-let cont = 1;
-let identificador = 0;
+// const ARRAY_CELDAS = Array.from(document.getElementsByTagName('td'));
+// let cont = 1;
+// let identificador = 0;
 
-function pintar() {
-    this.style.backgroundColor="cornflowerblue";
-    cont ++;
-}
+// function pintar() {
+//     cont ++;
+//     this.innerHTML= cont + " - " + this.id;
+//     this.style.backgroundColor="cornflowerblue";
+// }
 
-ARRAY_CELDAS.forEach(celda => {
-    celda.id = `idCeldas${identificador}`;
-    let datoInner = celda.innerHTML;
-    celda.addEventListener('dblclick', pintar);
-})
+// ARRAY_CELDAS.forEach(celda => {
+//     celda.id = `idCeldas${identificador}`;
+//     let datoInner = celda.innerHTML;
+//     celda.addEventListener('dblclick', pintar);
+// })
