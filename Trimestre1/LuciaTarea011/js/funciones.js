1f0a3e3006da2730d245e4e310d32569b4489545
Lucia <lucia.matest@educa.jcyl.es>
2022-11-08 17:11:02 +0000

    1

--- Trimestre1/LuciaTarea011/js/funciones.js
+++
@@ -1,9 +0,0 @@
-let velocidad = 10;
-let xDiana = 50;
-
-function desplazar() {
-    xDiana+=velocidad;
-    document.getElementById('diana').style.left = `${xDiana}px`
-}
-
-let intervalDiana = setInterval(desplazar, 50);
