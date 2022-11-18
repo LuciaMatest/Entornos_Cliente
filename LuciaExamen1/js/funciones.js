0af71d36b1800e494cb5f0859e55b21cd5d58925
Lucia <lucia.matest@educa.jcyl.es>
2022-11-18 10:55:51 +0000

    1

--- LuciaExamen1/js/funciones.js
+++ LuciaExamen1/js/funciones.js
@@ -19,20 +19,20 @@ divLista.appendChild(primeraLista);
 divLista.appendChild(segundaLista);
 
 // PARTE 2
-let listaOrdenada = [];
-let listaDesordenada = [];
+let listaDesordenada = Array.from(document.getElementsByTagName("OL"));
+let listaOrdenada = Array.from(document.getElementsByTagName("UL"));
 
-let btnSO = document.createElement('input');
+let btnSO = document.createElement('button');
 btnSO.type = 'radio';
 btnSO.id = 'sinOrdenar';
 btnSO.innerText = "Sin ordenar";
 
-let btnNombre = document.createElement('input');
+let btnNombre = document.createElement('button');
 btnNombre.type = 'radio';
 btnNombre.id = 'porNombre';
 btnNombre.innerText = 'Por nombre';
 
-let btnHabitantes = document.createElement('input');
+let btnHabitantes = document.createElement('button');
 btnHabitantes.type = 'radio';
 btnHabitantes.id = 'porHabitantes';
 btnHabitantes.innerText = 'Por habitantes';
@@ -40,3 +40,11 @@ btnHabitantes.innerText = 'Por habitantes';
 document.body.appendChild(btnHabitantes);
 document.body.appendChild(btnNombre);
 document.body.appendChild(btnSO);
+
+btnNombre.addEventListener('click', function (divLista) {
+    listaDesordenada.sort();
+});
+
+btnHabitantes.addEventListener('click', function (divLista) {
+    listaOrdenada.sort();
+});
