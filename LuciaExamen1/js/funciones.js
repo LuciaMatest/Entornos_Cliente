e7c9054432ba458cb8aa53f03a2c21bb6640723b
Lucia <lucia.matest@educa.jcyl.es>
2022-11-24 08:04:35 +0000

    1

--- LuciaExamen1/js/funciones.js
+++ LuciaExamen1/js/funciones.js
@@ -1,60 +1,61 @@
 "use strict";
 // PARTE 1
-document.body.style.backgroundColor = 'cornflowerblue';
-document.body.style.textAlign = 'center';
+document.body.style.backgroundColor = "cornflowerblue";
+document.body.style.textAlign = "center";
 
 function crearDivPrincipal() {
-    let divLista = document.createElement('div');
-    divLista.setAttribute('style', 'display: flex; justify-content: center');
+  let divLista = document.createElement("div");
+  divLista.setAttribute("style", "display: flex; justify-content: center");
 
-    let primeraLista = document.getElementById('idCiudades');
-    let segundaLista = document.getElementById('idHabitantes');
-    primeraLista.parentElement.insertBefore(divLista, primeraLista);
+  let primeraLista = document.getElementById("idCiudades");
+  let segundaLista = document.getElementById("idHabitantes");
+  primeraLista.parentElement.insertBefore(divLista, primeraLista);
 
-    divLista.appendChild(primeraLista);
-    divLista.appendChild(segundaLista);
-    primeraLista.style = 'textAlign: left';
-    segundaLista.style= 'textAlign: right';
-
-    // let footer= document.getElementsByTagName("footer")[0];
-    
-    // document.body.appendChild(divLista);
-    // document.body.appendChild(footer);
+  divLista.appendChild(primeraLista);
+  divLista.appendChild(segundaLista);
+  primeraLista.style = "text-align: left";
+  segundaLista.style = "text-align: right";
 }
 
 function crearArrayCiudades(listaNombres, listaHabitantes) {
-    let ciudades = [];
-    return ciudades;
+  let ciudades = [];
+  return ciudades;
+  
 }
 
+let listaNombres = document.getElementById("idCiudades").querySelectorAll("li");
+let listaHabitantes = document.getElementById("idHabitantes").querySelectorAll("li");
 
-// PARTE 2
-let listaDesordenada = Array.from(document.getElementsByTagName("OL"));
-let listaOrdenada = Array.from(document.getElementsByTagName("UL"));
+crearDivPrincipal();
 
-let btnSO = document.createElement('button');
-btnSO.type = 'radio';
-btnSO.id = 'sinOrdenar';
-btnSO.innerText = "Sin ordenar";
 
-let btnNombre = document.createElement('button');
-btnNombre.type = 'radio';
-btnNombre.id = 'porNombre';
-btnNombre.innerText = 'Por nombre';
+// // PARTE 2
+// let listaDesordenada = Array.from(document.getElementsByTagName("OL"));
+// let listaOrdenada = Array.from(document.getElementsByTagName("UL"));
 
-let btnHabitantes = document.createElement('button');
-btnHabitantes.type = 'radio';
-btnHabitantes.id = 'porHabitantes';
-btnHabitantes.innerText = 'Por habitantes';
+// let btnSO = document.createElement('button');
+// btnSO.type = 'radio';
+// btnSO.id = 'sinOrdenar';
+// btnSO.innerText = "Sin ordenar";
 
-document.body.appendChild(btnHabitantes);
-document.body.appendChild(btnNombre);
-document.body.appendChild(btnSO);
+// let btnNombre = document.createElement('button');
+// btnNombre.type = 'radio';
+// btnNombre.id = 'porNombre';
+// btnNombre.innerText = 'Por nombre';
 
-btnNombre.addEventListener('click', function (divLista) {
-    listaDesordenada.sort();
-});
+// let btnHabitantes = document.createElement('button');
+// btnHabitantes.type = 'radio';
+// btnHabitantes.id = 'porHabitantes';
+// btnHabitantes.innerText = 'Por habitantes';
 
-btnHabitantes.addEventListener('click', function (divLista) {
-    listaOrdenada.sort();
-});
+// document.body.appendChild(btnHabitantes);
+// document.body.appendChild(btnNombre);
+// document.body.appendChild(btnSO);
+
+// btnNombre.addEventListener('click', function (divLista) {
+//     listaDesordenada.sort();
+// });
+
+// btnHabitantes.addEventListener('click', function (divLista) {
+//     listaOrdenada.sort();
+// });
