b97fff9bf414435ee926b4675dcf4a983eec1333
Lucia <lucia.matest@educa.jcyl.es>
2022-12-01 08:48:14 +0000

    1

--- Trimestre1/Sintaxis 3/objetos/objetos.js
+++ Trimestre1/Sintaxis 3/objetos/objetos.js
@@ -8,8 +8,17 @@ let alumna = {
     nombre: 'Ana',
     apellidos: 'Cifuentes Martin',
     edad: 26,
+    medidas: {
+        altura: 1.7,
+        pie: 32,
+    },
 };
 
 console.log(alumna.edad + 1);
 alumna.edad = 40;
 console.log(alumna.edad);
+let edad = 'edad';
+console.log(alumna[edad]);
+
+console.log(alumna);
+console.log(alumna.medidas.altura);
