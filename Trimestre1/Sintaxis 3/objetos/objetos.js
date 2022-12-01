2a410bcc207b667042ace630c2e1125a4f80a2f1
Lucia <lucia.matest@educa.jcyl.es>
2022-12-01 21:13:33 +0000

    1

--- Trimestre1/Sintaxis 3/objetos/objetos.js
+++ Trimestre1/Sintaxis 3/objetos/objetos.js
@@ -1,24 +1,67 @@
-let alumno = new Object();
-alumno.nombre = 'Carlos';
-alumno['apellidos'] = 'Perez Ortiz';
-alumno.edad = 19;
+// let alumno = new Object();
+// alumno.nombre = 'Carlos';
+// alumno['apellidos'] = 'Perez Ortiz';
+// alumno.edad = 19;
 
-let alumna = {
-    // propiedades
-    nombre: 'Ana',
-    apellidos: 'Cifuentes Martin',
-    edad: 26,
-    medidas: {
-        altura: 1.7,
-        pie: 32,
-    },
-};
+// let alumna = {
+//     // propiedades
+//     nombre: 'Ana',
+//     apellidos: 'Cifuentes Martin',
+//     edad: 26,
+//     medidas: {
+//         altura: 1.7,
+//         pie: 32,
+//     },
+// };
 
-console.log(alumna.edad + 1);
-alumna.edad = 40;
-console.log(alumna.edad);
-let edad = 'edad';
-console.log(alumna[edad]);
+// console.log(alumna.edad + 1);
+// alumna.edad = 40;
+// console.log(alumna.edad);
+// let edad = 'edad';
+// console.log(alumna[edad]);
 
-console.log(alumna);
-console.log(alumna.medidas.altura);
+// console.log(alumna);
+// console.log(alumna.medidas.altura);
+//-----------------------------------------------------------------------------------------------
+// console.clear();
+// Number.prototype.moneda=function () {
+//     return this.toLocaleString('es-ES',{style: 'currency',currency: 'EUR'});
+// }
+
+// String.prototype.mayusculas=function () {
+//     return this.toLocaleUpperCase();
+// }
+
+// const tvSamsung ={
+//     nombre: 'TV Samsung 42"',
+//     categoria: 'Televisores',
+//     unidades: 4,
+//     precio: 345.95,
+
+//     importe: function () {
+//         return `${(this.nombre).mayusculas()} : ${(this.unidades*this.precio).moneda()}`;
+//     }
+
+// }
+
+// console.log(tvSamsung.importe());
+//-----------------------------------------------------------------------------------------------
+Number.prototype.moneda = function () {
+    return this.toLocaleString('de-DE',{style: 'currency',currency: 'EUR'});
+}
+
+class Productos {
+    constructor(nombre, categoria, unidades, precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.unidades = unidades;
+        this.precio = precio;
+    }
+    getInfo() {
+        return `${this.nombre} (${this.categoria}): ${this.unidades} uds x ${this.precio.moneda()} = ${(this.unidades*this.precio).moneda()}`;
+    }
+}
+
+let cpo = new Productos('Asus', 'Ordenador', 3, 367.8);
+console.log(cpo.getInfo());
+//-----------------------------------------------------------------------------------------------
