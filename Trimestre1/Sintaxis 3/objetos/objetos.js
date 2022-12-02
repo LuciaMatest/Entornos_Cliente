91c826c0456334554de5f5868ba52fdb70d65bc0
Lucia <lucia.matest@educa.jcyl.es>
2022-12-02 08:31:51 +0000

    1

--- Trimestre1/Sintaxis 3/objetos/objetos.js
+++ Trimestre1/Sintaxis 3/objetos/objetos.js
@@ -65,3 +65,58 @@ class Productos {
 let cpo = new Productos('Asus', 'Ordenador', 3, 367.8);
 console.log(cpo.getInfo());
 //-----------------------------------------------------------------------------------------------
+//     getInfo2: () =>{
+//         console.log(this);
+//         return this;
+//     }
+//     getInfo2: function () {
+//         return `${this.nombre} mide ${this.medidas?.altura} cm`;
+//     }
+// };
+
+// console.log(alumna.edad + 1);
+// alumna.edad = 40;
+// console.log(alumna.edad);
+// let edad = 'edad';
+// console.log(alumna[edad]);
+
+// console.log(alumna);
+// console.log(alumna.medidas.altura);
+// console.log(alumna.medidas.pie);
+
+// console.log(alumna.notas?.primero);
+
+// for (let propiedades in alumna) {
+//     for (let key in propiedades) {
+//         console.log(`${propiedades}: ${alumna[propiedades]}`);
+//     }
+// }
+
+// alumna.colorGorro = 'verde';
+// console.log(Object.values(alumna));
+
+// metodo
+// alumna.getInfo = function () {
+//     return `${this.nombre} tiene ${this.edad} años`;
+// }
+
+// console.log(alumna.getInfo());
+// console.log(alumna.getInfo2());
+
+String.prototype.mayusculas = function () {
+    return this.toLocaleUpperCase();
+}
+
+let tvSamsung = {
+    nombre: 'TV Samsung 42"',
+    categoria: 'televisores',
+    unidades: 4,
+    precio: 345.95,
+    importe: function() {
+        let total = this.unidades*this.precio;
+        return `${this.nombre}: ${total.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}`;
+    }
+};
+
+console.log(tvSamsung.importe());
+console.log(`${tvSamsung.nombre.mayusculas()}`);
