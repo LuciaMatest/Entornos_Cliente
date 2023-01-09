b7268c007d7972c790ae41ba8ffd016fee98ba7b
Lucia <lucia.matest@educa.jcyl.es>
2023-01-09 13:21:56 +0000

    1

--- Trimestre1/Sintaxis 9/js/funciones.js
+++ Trimestre1/Sintaxis 9/js/funciones.js
@@ -1,4 +1,35 @@
-document.cookie = "nombre=Paco";
-function obtenerValor(nombreCookie) {
-  alert(document.cookie);
-}
+// document.cookie = "nombreCookie=Paco";
+
+
+// function obtenerValor(nombreCookie){
+//     let cokies= document.cookie;
+//     //Divido por espacios
+//     let cadena= cokies.split(" ");
+
+//     for (const iterator of cadena) {
+//         //Divido por ;
+//         let p= iterator.split(";");
+//         for (const i of p) {
+//             let f=i.split("=")
+//             for (let u = 0; u < f.length; u++) {
+//                 if(f[u]==nombreCookie){
+//                     console.log("VALOR FINAL "+ f[++u]);
+//                 }
+//             }          
+//         }
+//     }
+// }
+
+// valorCookie("nombreCookie");
+
+let c1 ='uno';
+let c2 ='dos';
+document.cookie = "c1="+encodeURIComponent(c1);
+document.cookie = "c2="+encodeURIComponent(c2);
+document.cookie = "c3="+encodeURIComponent(c3);
+console.log(document.cookie);
+let array1 = document.cookie.split(';');
+let colletionC = new Map();
+array1.forEach((c)=>{
+    let array2 = c.split('=');
+});
