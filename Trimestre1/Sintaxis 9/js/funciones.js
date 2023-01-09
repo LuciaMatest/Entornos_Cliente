c6e9a7ab4bee067ae8d7cf8986f7f495d10aa619
Lucia <lucia.matest@educa.jcyl.es>
2023-01-09 12:38:19 +0000

    1

--- Trimestre1/Sintaxis 9/js/funciones.js
+++ Trimestre1/Sintaxis 9/js/funciones.js
@@ -1,6 +1,10 @@
 function obtenerValor(nombreCookie) {
     let nombreCookie = obtenerCookie(nombreCookie); 
-    if (nombreCookie!="")
+    if (nombreCookie!=""){
+
+
+    }
 }
 
-document.cookie = 'nombreCookie=' + obtenerValor(prueba);
+document.cookie = 'nombre=' + obtenerValor(prueba);
+
