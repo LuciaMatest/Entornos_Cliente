0219493ccad2c2a1ba02c8f8680e345919163198
Lucia <lucia.matest@educa.jcyl.es>
2023-01-16 12:09:05 +0000

    2

---
+++ LuciaExamen2/js/animales.js
@@ -0,0 +1,12 @@
+
+// a) Poner colores de fondo distintos para cada apartado. Poner Font-family : “arial” para los 4 apartados.
+let div01 = document.getElementById('div01');
+div01.setAttribute('style', 'background-color: cadetblue; font-family: Arial, Helvetica, sans-serif;');
+let div02 = document.getElementById('div02');
+div02.setAttribute('style', 'background-color: lightblue; font-family: Arial, Helvetica, sans-serif;');
+let div03 = document.getElementById('div03');
+div03.setAttribute('style', 'background-color: burlywood; font-family: Arial, Helvetica, sans-serif;');
+let div04 = document.getElementById('div04');
+div04.setAttribute('style', 'background-color: darkseagreen; font-family: Arial, Helvetica, sans-serif;');
+
+// b) Crear un array de 6 elementos con las propiedades que se observan en la “tabla de animales”. Se puede decir que será un array de animales, o un array de objetos animales.
