2020467f09e73b9d77fafe634cd2101c7bd369bd
Lucia <lucia.matest@educa.jcyl.es>
2023-02-05 11:15:06 +0000

    1

--- Trimestre1/10LuciaTarea018/JS/render.js
+++ Trimestre1/10LuciaTarea018/JS/render.js
@@ -1,62 +1,58 @@
 const SERVER = 'http://192.168.0.214:3000';
 
-$('#listar').click(async function (e) { 
-    e.preventDefault();
-    
-    const [catergorias,productos]= await Promise.all([
-        getTabla('/categorias'),
-        getTabla('/productos')
-    ])
-
-    catergorias.forEach((catergoria) => renderCategoria(catergoria));
-    productos.forEach((producto) => renderProducto(producto));
-
-    $('#borrar').removeAttr("disabled");
-    $(this).attr("disabled",true);
-
+window.addEventListener('load', ()=>{
+    //Ordenar
+    document.getElementById('list').addEventListener('submit', async function(evento) {
+        //Cancelas la accion predeterminada
+        evento.preventDefault();
+        //Pasamos un array de promesas
+        const [categorias, productos] = await Promise.all([
+            getTabla('/categorias'),
+            getTabla('/productos')
+        ]);
+        //Procesamos los datos
+        categorias.forEach((categoria) => renderCategory(categoria));
+        productos.forEach((producto) => renderProduct(producto));
+    })
 });
 
+//Recogemos los datos de cada tabla
 async function getTabla(tabla) {
     const response = await fetch(`${SERVER}${tabla}`);
-
-    if (!response.ok){
-        throw `Error ${response.status}de la BBDD: ${response.statusText}`
+    //Comprobamos si se ha resuelto
+    if (!response.ok) {
+        //Lanzamos un error
+        throw `Error ${response.status} de la BBDD: ${response.statusText}`;
     }
+    //Devolvemos los datos JSON
+    const dato = await response.json()
+    return dato;
+};
 
-    const datos = await response.json();
+//Creamos dos funciones para llamar tanto a los productos como a las categorias
+function renderCategory(categoria) {
+    let liCategoria = document.createElement('li');
+    liCategoria.setAttribute('class','list-group-item');
+    liCategoria.id = categoria.id;
+    liCategoria.appendChild(document.createTextNode(categoria.name));
 
-    return datos;
+    let olCategoria = document.getElementById('listados');
+    olCategoria.appendChild(liCategoria);
 }
 
-function renderCategoria(cat) {
-    let primerNivel=document.createElement('li');
-    primerNivel.appendChild(document.createTextNode(cat.name));
-
-    let segundoNivel=document.createElement('ol');
-    segundoNivel.setAttribute('id',cat.id);
-    primerNivel.appendChild(segundoNivel);
-
-    document.getElementById('categorias').appendChild(primerNivel);
-}
-
-function renderProducto(prod) {
-    let elemento= document.createElement('li');
-    elemento.appendChild(document.createTextNode(prod.name));
-
-    if (prod.cat==1){
-        document.getElementById('1').appendChild(elemento);
-    }else if(prod.cat==2){
-        document.getElementById('2').appendChild(elemento);
+function renderProduct(producto) {
+    let liProducto = document.createElement('li');
+    liProducto.setAttribute('class','list-group-item');
+    liProducto.appendChild(document.createTextNode(producto.name));
+    //Dependiendo de la categoria en la que se encuentre se irá a una lista u otra
+    if (producto.cat==document.getElementById("1").id) {
+        let olProd1 = document.createElement('ol');
+        olProd1.appendChild(liProducto)
+    } else if(producto.cat==document.getElementById("2").id){
+        let olProd2 = document.createElement('ol');
+        olProd2.appendChild(liProducto)
     }
+    document.getElementById("1").appendChild(olProd1);
+    document.getElementById("2").appendChild(olProd2);
 }
 
-//---------------------------------------------------------------------------
-
-$('#borrar').click(function (e) { 
-    e.preventDefault();
-    
-    document.getElementById('categorias').innerHTML='';
-
-    $(this).attr("disabled",true);
-    $('#listar').removeAttr("disabled");
-});
