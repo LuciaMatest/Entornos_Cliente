0b9b25b07a5e73244eebf98c288e9c028638628e
Lucia <lucia.matest@educa.jcyl.es>
2023-02-05 11:45:48 +0000

    1

--- Trimestre1/10LuciaTarea018/JS/render.js
+++ Trimestre1/10LuciaTarea018/JS/render.js
@@ -31,27 +31,23 @@ async function getTabla(tabla) {
 
 //Creamos dos funciones para llamar tanto a los productos como a las categorias
 function renderCategory(categoria) {
-    let liCategoria = document.createElement('li');
-    liCategoria.setAttribute('class','list-group-item');
-    liCategoria.id = categoria.id;
-    liCategoria.appendChild(document.createTextNode(categoria.name));
+    let olCategoria = document.createElement('ol');
+    olCategoria.setAttribute('class','list-group-item');
+    olCategoria.setAttribute('id',categoria.id);
+    olCategoria.appendChild(document.createTextNode(categoria.name));
 
-    document.getElementById('listados').appendChild(liCategoria);
+    document.getElementById('listados').appendChild(olCategoria);
 }
 
 function renderProduct(producto) {
     let liProducto = document.createElement('li');
-    liProducto.setAttribute('class','list-group-item');
+    liProducto.setAttribute('class','list-group-item p-3');
     liProducto.appendChild(document.createTextNode(producto.name));
     //Dependiendo de la categoria en la que se encuentre se irá a una lista u otra
-    if (producto.cat==document.getElementById("1").id) {
-        let olProd1 = document.createElement('ol');
-        olProd1.appendChild(liProducto)
-    } else if(producto.cat==document.getElementById("2").id){
-        let olProd2 = document.createElement('ol');
-        olProd2.appendChild(liProducto)
+    if (producto.cat==1) {
+        document.getElementById('1').appendChild(liProducto);
+    } else if(producto.cat==2){
+        document.getElementById('2').appendChild(liProducto);
     }
-    document.getElementById('listados').appendChild(olProd1);
-    document.getElementById('listados').appendChild(olProd2);
 }
 
