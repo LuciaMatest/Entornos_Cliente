e9c33c95526edf7a6a2d77cdb4016bef9174c1a7
Lucia <lucia.matest@educa.jcyl.es>
2023-02-10 11:37:44 +0000

    1

--- ExamenFebreroLucia/Ejercicio4/js/crear_entidades.js
+++ ExamenFebreroLucia/Ejercicio4/js/crear_entidades.js
@@ -1,1 +1,6 @@
 const SERVER="http://192.168.2.204:3000/";
+
+const botonRepartir= document.createElement('button');
+botonRepartir.innerHTML='Repartir';
+
+document.getElementById('div03').appendChild(botonRepartir);
