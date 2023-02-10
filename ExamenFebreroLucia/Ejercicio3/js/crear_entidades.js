a95b52dc2e1cc32051d3804226de0f1b7fb136a1
Lucia <lucia.matest@educa.jcyl.es>
2023-02-10 11:35:55 +0000

    1

--- ExamenFebreroLucia/Ejercicio3/js/crear_entidades.js
+++ ExamenFebreroLucia/Ejercicio3/js/crear_entidades.js
@@ -2,9 +2,12 @@ const SERVER="http://192.168.2.204:3000/";
 
 const boton= document.createElement('button');
 boton.innerHTML='Rellenar';
+let formulario=document.getElementById('forma');
 
-let arrayPalos=['OROS','COPAS','ESPADAS','BASTOS'];
-let arrayValores=[1,2,3,4,5,6,7,10,11,12];
-let arrayLugares=['Baraja','Descartes','Jugador1','Jugador2','Jugador3','Jugador4']
+formulario.addEventListener('submit', function(evento) {
+    
+})
 
 document.getElementById('forma').appendChild(boton);
+
+document.getElementById('div04').innerHTML=`Ha elegido ${} de ${}`;
