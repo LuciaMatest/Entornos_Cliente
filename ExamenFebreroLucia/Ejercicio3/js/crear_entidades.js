8bb89d1f98aae360b2b7a6a4287c871968268182
Lucia <lucia.matest@educa.jcyl.es>
2023-02-10 11:28:43 +0000

    1

--- ExamenFebreroLucia/Ejercicio3/js/crear_entidades.js
+++ ExamenFebreroLucia/Ejercicio3/js/crear_entidades.js
@@ -5,5 +5,6 @@ boton.innerHTML='Rellenar';
 
 let arrayPalos=['OROS','COPAS','ESPADAS','BASTOS'];
 let arrayValores=[1,2,3,4,5,6,7,10,11,12];
+let arrayLugares=['Baraja','Descartes','Jugador1','Jugador2','Jugador3','Jugador4']
 
-document.getElementById('div04').appendChild(boton);
+document.getElementById('forma').appendChild(boton);
